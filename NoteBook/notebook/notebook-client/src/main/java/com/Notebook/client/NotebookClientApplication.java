3a04c4a9e1a9add0aa0ed501e1a4e1f4c44c93d1
Gianni Parello <giparello90@gmail.com>
2022-11-25 11:54:02 -0500

    setup

---
+++ NoteBook/notebook/notebook-client/src/main/java/com/Notebook/client/NotebookClientApplication.java
@@ -0,0 +1,13 @@
+package com.Notebook.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookClientApplication.class, args);
+	}
+
+}
